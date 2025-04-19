a33264694dd9dacfcaeca0af1818091a72e4760a
Damian Mostert <damianmostert86@gmail.com>
2025-04-19 18:28:37 +0200

    update

--- client/client.tsx
+++ client/client.tsx
@@ -6,11 +6,11 @@ document.querySelectorAll('script[nestclient]').forEach((script) => {
 		const { body, props = {}, type = 'div', id } = json;
 		if (!id || !body) return;
 		const Component = new Function('React', `return (${body})`)(React);
-		const Wrapper = () => React.createElement(React.Fragment, null, <Component {...props} />);
+		const Wrapper = () => React.createElement(React.Fragment, null, Component(props));
 		const target = document.getElementById(id);
 		if (target) {
 			const root = ReactDOM.createRoot(target);
-        root.render(<Wrapper />);
+        root.render(Wrapper());
 		}
 		script.remove();
 	} catch (e) {
